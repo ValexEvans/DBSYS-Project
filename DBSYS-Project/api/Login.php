725cb630ae54e26dec276a3c035c0f06a165bc39
ValexEvans <57923977+ValexEvans@users.noreply.github.com>
2024-02-20 16:55:53 -0500

    changed login php

--- DBSYS-Project/api/Login.php
+++ DBSYS-Project/api/Login.php
@@ -1,56 +1,57 @@
 <?php
-	$inData = getRequestInfo();
-	
-	$UserID = 0;
-	$firstName = "";
-	$lastName = "";
+    $inData = getRequestInfo();
+    
+    $UserID = 0;
+    $firstName = "";
+    $lastName = "";
+    $Email = "";
+    $Role = "";
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331", "COP4331"); 	
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("SELECT UserID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
-		$stmt->execute();
-		$result = $stmt->get_result();
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "DB01");
+    if( $conn->connect_error )
+    {
+        returnWithError( $conn->connect_error );
+    }
+    else
+    {
+        $stmt = $conn->prepare("SELECT UserID, firstName, lastName, Email, Role FROM Users WHERE Login=? AND Password =?");
+        $stmt->bind_param("ss", $inData["login"], $inData["password"]);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
-		if( $row = $result->fetch_assoc()  )
-		{
-			returnWithInfo( $row['firstName'], $row['lastName'], $row['UserID'] );
-		}
-		else
-		{
-			returnWithError("No Records Found");
-		}
+        if( $row = $result->fetch_assoc() )
+        {
+            returnWithInfo($row['UserID'], $row['firstName'], $row['lastName'], $row['Email'], $row['Role']);
+        }
+        else
+        {
+            returnWithError("No Records Found");
+        }
 
-		$stmt->close();
-		$conn->close();
-	}
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+        $stmt->close();
+        $conn->close();
+    }
+    
+    function getRequestInfo()
+    {
+        return json_decode(file_get_contents('php://input'), true);
+    }
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"UserID":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $firstName, $lastName, $UserID )
-	{
-		$retValue = '{"UserID":' . $UserID . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+    function sendResultInfoAsJson( $obj )
+    {
+        header('Content-type: application/json');
+        echo $obj;
+    }
+    
+    function returnWithError( $err )
+    {
+        $retValue = '{"UserID":0,"firstName":"","lastName":"","Email":"","Role":"","error":"' . $err . '"}';
+        sendResultInfoAsJson( $retValue );
+    }
+    
+    function returnWithInfo( $UserID, $firstName, $lastName, $Email, $Role )
+    {
+        $retValue = '{"UserID":' . $UserID . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","Email":"' . $Email . '","Role":"' . $Role . '","error":""}';
+        sendResultInfoAsJson( $retValue );
+    }
 ?>
